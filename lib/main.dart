44b32c5677e0a54837cf4a4692165ab0ef91e670
neryad <dayern.gomez@hotmail.com>
2020-07-10 15:56:57 -0400

    HomeWork Done

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 
 void main() {
-  runApp(calApp());
+  runApp(CalApp());
 }
 
-class calApp extends StatelessWidget {
-  //const calApp({Key key}) : super(key: key);
-
+class CalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,17 +35,15 @@ class _SimpleCalState extends State<SimpleCal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor ,
-      //appBar: AppBar(title: Text('asdasdasd')),
+      backgroundColor: bgColor,
       body: Column(
-        
         children: <Widget>[
           Expanded(
-            
-             child: Divider( color: bgColor,),
+            child: Divider(
+              color: bgColor,
+            ),
           ),
-            Container(
-            
+          Container(
             alignment: Alignment.centerRight,
             padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
             child: Text(
@@ -55,21 +51,21 @@ class _SimpleCalState extends State<SimpleCal> {
               style: TextStyle(fontSize: equationSize, color: bgColor1),
             ),
           ),
-             Container(
-            
+          Container(
             alignment: Alignment.centerRight,
             padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
             child: Text(
               result,
-              style: TextStyle(fontSize: resultSize,color: bgColor1, fontWeight: FontWeight.w600),
+              style: TextStyle(
+                  fontSize: resultSize,
+                  color: bgColor1,
+                  fontWeight: FontWeight.w600),
             ),
           ),
           Container(
             height: 420,
-
             color: bgColor2,
             child: Row(
-            
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Container(
@@ -78,7 +74,7 @@ class _SimpleCalState extends State<SimpleCal> {
                   child: Table(children: [
                     TableRow(children: [
                       _buildBtn("C", 1, bgColor, bgColor2),
-                      _buildBtn("⌫", 1,bgColor, bgColor2),
+                      _buildBtn("+/-", 1, bgColor, bgColor2),
                       _buildBtn("%", 1, bgColor, bgColor2),
                     ]),
                     TableRow(children: [
@@ -97,9 +93,9 @@ class _SimpleCalState extends State<SimpleCal> {
                       _buildBtn("3", 1, bgColor2, bgColor1),
                     ]),
                     TableRow(children: [
-                      _buildBtn(".", 1, bgColor2,bgColor1),
-                      _buildBtn("0", 1, bgColor2,bgColor1),
-                      _buildBtn("00", 1, bgColor2,bgColor1),
+                      _buildBtn("0", 1, bgColor2, bgColor1),
+                      _buildBtn("", 1, bgColor2, bgColor1),
+                      _buildBtn(".", 1, bgColor2, bgColor1),
                     ]),
                   ]),
                 ),
@@ -107,12 +103,16 @@ class _SimpleCalState extends State<SimpleCal> {
                   width: MediaQuery.of(context).size.width * 0.20,
                   child: Table(
                     children: [
-                      
-                      TableRow(children: [_buildBtn('/', 1, bgColor,bgColor2)]),
-                      TableRow(children: [_buildBtn('×', 1, bgColor,bgColor2)]),
-                      TableRow(children: [_buildBtn('-', 1, bgColor,bgColor2)]),
-                      TableRow(children: [_buildBtn('+', 1, bgColor,bgColor2)]),
-                      TableRow(children: [_buildBtn('=', 1, bgColor1, bgColor2)]),
+                      TableRow(
+                          children: [_buildBtn('/', 1, bgColor, bgColor2)]),
+                      TableRow(
+                          children: [_buildBtn('×', 1, bgColor, bgColor2)]),
+                      TableRow(
+                          children: [_buildBtn('-', 1, bgColor, bgColor2)]),
+                      TableRow(
+                          children: [_buildBtn('+', 1, bgColor, bgColor2)]),
+                      TableRow(
+                          children: [_buildBtn('=', 1, bgColor1, bgColor2)]),
                     ],
                   ),
                 ),
@@ -124,20 +124,19 @@ class _SimpleCalState extends State<SimpleCal> {
     );
   }
 
-  Widget _buildBtn(String btnText, double btnHeight, Color bntColor, Color btnCText) {
+  Widget _buildBtn(
+      String btnText, double btnHeight, Color bntColor, Color btnCText) {
     return Container(
         height: MediaQuery.of(context).size.height * 0.1 * btnHeight,
         padding: EdgeInsets.all(10),
-        
         child: FlatButton(
           color: bntColor,
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10.0),
               side: BorderSide(
-                  color:bntColor, width: 1, style: BorderStyle.solid)
-                  ),
-          child: Text(btnText,
-              style: TextStyle(fontSize: 30.0, color: btnCText)),
+                  color: bntColor, width: 1, style: BorderStyle.solid)),
+          child:
+              Text(btnText, style: TextStyle(fontSize: 30.0, color: btnCText)),
           onPressed: () => btnPres(btnText),
         ));
   }
@@ -147,9 +146,8 @@ class _SimpleCalState extends State<SimpleCal> {
       if (btnText == "C") {
         equation = "0";
         result = "0";
-      } else if (btnText == "⌫") {
-        equation = equation.substring(0, equation.length - 1);
-
+      } else if (btnText == "+/-") {
+        equation = (int.parse(equation) * -1).toString();
         if (equation == "") {
           equation = "0";
         }
@@ -157,6 +155,7 @@ class _SimpleCalState extends State<SimpleCal> {
         expresion = equation;
         expresion = equation.replaceAll('x', '*');
         expresion = equation.replaceAll('/', '/');
+        expresion = equation.replaceAll('%', '%');
 
         try {
           Parser p = Parser();
